b5d2d8abcbc384af6e217d74be4a08c548b514c7
ochoo <395174+ganoch@users.noreply.github.com>
2021-04-30 17:24:44 +0800

    fix(psr): missing dependency

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -7,12 +7,10 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php' ]; then
-	mkdir -p var/cache var/log public/media
 
 	if [ "$APP_ENV" != 'prod' ]; then
 		composer dump-autoload -o
 		composer install --prefer-dist --no-progress --no-suggest --no-interaction
-		bin/console assets:install --no-interaction
 	fi
 fi
 
